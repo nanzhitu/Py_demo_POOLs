356bbfece60b00b83ff5293dfc3bd132f0037c1e
neo-gj.ye <neo-gj.ye@mstarsemi.com>
2017-04-10 22:48:47 +0800

    add quick sort

--- 000sort/sort.py
+++ 000sort/sort.py
@@ -43,7 +43,32 @@ def bubble_sort(lists):
                 lists[i], lists[j] = lists[j], lists[i]
     return lists
 
-LISTS = [3, 5, 1, 7, 3, 11, 89, 34]
+def quick_sort(lists, left, right):
+    '''快速排序'''
+    if left >= right:
+        return lists
+    key = lists[left]
+    low = left
+    high = right
+    while left < right:
+        while left < right and lists[right] >= key:
+            right -= 1
+        lists[left] = lists[right]
+        while left < right and lists[left] <= key:
+            left += 1
+        lists[right] = lists[left]
+    lists[right] = key
+    print lists
+    print left, right
+    print low, high
+    quick_sort(lists, low, left - 1)
+    quick_sort(lists, left + 1, high)
+    return lists
+
+
+LISTS = [3, 5, 1, 7, 2, 11, 89, 34]
 #print insert_sort(LISTS)
 #print shell_sort(LISTS)
-print bubble_sort(LISTS)
+#print bubble_sort(LISTS)
+print LISTS
+print quick_sort(LISTS, 0, 7)
