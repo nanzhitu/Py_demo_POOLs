203fe997940951a3f439e90e630aad5998336610
neo-gj.ye <neo-gj.ye@mstarsemi.com>
2017-04-13 16:35:10 +0800

    add merge_sort

--- 000sort/sort.py
+++ 000sort/sort.py
@@ -76,10 +76,37 @@ def select_sort(lists):
         lists[min_], lists[i] = lists[i], lists[min_]
     return lists
 
+def merge(left, right):
+    '''合并'''
+    i, j = 0, 0
+    result = []
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    print left, right, result
+    return result
+
+def merge_sort(lists):
+    '''归并排序'''
+    if len(lists) <= 1:
+        return lists
+    num = len(lists) / 2
+    left = merge_sort(lists[:num])
+    right = merge_sort(lists[num:])
+    return merge(left, right)
+
+
 LISTS = [3, 5, 1, 7, 2, 11, 89, 34]
 #print insert_sort(LISTS)
 #print shell_sort(LISTS)
 #print bubble_sort(LISTS)
 print LISTS
 #print quick_sort(LISTS, 0, 7)
-print select_sort(LISTS)
+#print select_sort(LISTS)
+print merge_sort(LISTS)
